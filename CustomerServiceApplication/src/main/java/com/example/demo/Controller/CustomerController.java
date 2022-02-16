c31ff4d3106b68e06c5a58e00b1aa862f14e7a7d
Shenbaga722 <78945494+Shenbaga722@users.noreply.github.com>
2022-02-16 11:51:08 +0530

    Update CustomerController.java
    Final Commit

--- CustomerServiceApplication/src/main/java/com/example/demo/Controller/CustomerController.java
+++ CustomerServiceApplication/src/main/java/com/example/demo/Controller/CustomerController.java
@@ -10,50 +10,43 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
 @RestController
-@RequestMapping("/customerCreation")
 public class CustomerController {
     @Autowired
     CustomerRepo customerRepo;
-
     @Autowired
     RestTemplate restTemplate;
     @PostMapping("/createCustomerAccount")
     public ResponseEntity<CustomerAccount> createCustomer( @RequestBody CustomerAccount customer1) {
         try {
-            Customer customer = customerRepo.save(new Customer(customer1.getCustomer()));
-            Account account = new Account(customer1.getAccount().getAccountType(), customer1.getAccount().getIfscCode(), customer1.getAccount().getBranch());
+            Customer customer = customerRepo.save(new Customer(customer1.getCustomer().getCustomerName(),customer1.getCustomer().getAge(),customer1.getCustomer().getCity(),customer1.getCustomer().getEmailid()));
+            Account account = new Account(customer1.getAccount().getAccountType(), customer1.getAccount().getIFSCCode(),customer1.getAccount().getBranch());
             HttpHeaders header = new HttpHeaders();
             header.setContentType(MediaType.APPLICATION_JSON);
             HttpEntity<Account> httpEntity = new HttpEntity<>(account,header);
-            restTemplate.exchange(http://AccountServiceApplication/account/createAccounts,HttpMethod.POST,httpEntity,void.class);
+            restTemplate.exchange("http://AccountServiceApplication/account/createAccount",HttpMethod.POST,httpEntity,void.class);
             return new ResponseEntity<>(customer1, HttpStatus.CREATED);
-
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-
         }
-
     }
-
     @GetMapping("/getCustomers")
     public ResponseEntity<List<Customer>> getAllCustomers(@RequestParam(required = false) String name){
         try {
             List<Customer> customers = new ArrayList<>();
-            Customer customer =new Customer();
+
             if (name == null)
                 customerRepo.findAll().forEach(customers::add);
             else
-                customerRepo.findByCustomerName(name).forEach(customers::add);
+                customerRepo.findByName().forEach(customers::add);
+
             if (customers.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
+
             return new ResponseEntity<>(customers, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
-
